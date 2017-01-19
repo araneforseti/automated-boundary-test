05a9aee123eea8870856622cee662da77c5c8556
Sarah <sarahforseti@gmail.com>
2017-01-19 14:19:50 -0600

    Adding message name to field to support more customizable messages

--- src/main/groovy/org/araneforseti/boundary/fields/Field.groovy
+++ src/main/groovy/org/araneforseti/boundary/fields/Field.groovy
@@ -7,20 +7,17 @@ abstract class Field {
     String name
     boolean isRequired
     def correctValue
+    def messageName
 
-    Field(String name, correctValue, boolean required) {
+    Field(String name, correctValue, boolean required, String messageName="") {
         this.name = name
         this.isRequired = required
         this.correctValue = correctValue
+        this.messageName = messageName ?: name
     }
 
     List<BoundaryScenario> getCases() {
-        List<BoundaryScenario> scenarios = []
-
-        if (isRequired) {
-            scenarios.add(new BoundaryScenario(name, "${name} is a required field", null))
-        }
-        return scenarios
+        isRequired ? [new BoundaryScenario(messageName, "${messageName} is a required field", null)] : []
     }
 
     def getCorrectValue(fieldName="", value="") {
--- src/test/groovy/org/araneforseti/boundary/fields/FieldTest.groovy
+++ src/test/groovy/org/araneforseti/boundary/fields/FieldTest.groovy
@@ -6,12 +6,32 @@ import static org.araneforseti.boundary.TestUtil.scenarios_contains_value
 
 class FieldTest {
     @Test
-    public void required_fields_cannot_be_null() {
+    void required_fields_cannot_be_null() {
         assert scenarios_contains_value(null, new Field("testField", "foo", true) { })
     }
 
     @Test
-    public void nonrequired_field_can_be_null() {
+    void nonrequired_field_can_be_null() {
         assert !scenarios_contains_value(null, new Field("testField", "foo", false) { })
+    }
+
+    @Test
+    void field_can_have_message_name() {
+        assert (new TestField("testField", "foo", false, "message")).messageName == "message"
+    }
+
+    @Test
+    void field_message_name_defaults_to_name() {
+        assert (new TestField("testField", "foo", false)).messageName == "testField"
+    }
+
+    class TestField extends Field {
+        TestField(String name, Object correctValue, boolean required, String messageName) {
+            super(name, correctValue, required, messageName)
+        }
+
+        TestField(String name, Object correctValue, boolean required) {
+            super(name, correctValue, required)
+        }
     }
 }
